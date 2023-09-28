c1aab2fa1649df2270f456dcacfe07db2529af02
Rishi Basdeo <32602245+katahar@users.noreply.github.com>
2023-09-28 14:34:21 -0400

    final version

--- filters.py
+++ filters.py
@@ -26,7 +26,30 @@ import numpy as np
 
 def sharpen(img, kernel, k):
     img_out = cv2.filter2D(img, -1, kernel*k )
+
     return img_out
+
+def sharpen3o(img, k):
+    tmp_ortho = sharpen3x3ortho
+    tmp_ortho[1][1] = 4
+    tmp_ortho = tmp_ortho * k
+    tmp_ortho[1][1] = tmp_ortho[1][1]+5
+    tmp_ortho = tmp_ortho /5
+    img_out = cv2.filter2D(img, -1, tmp_ortho )
+
+    return img_out
+
+def sharpen3a(img, k):
+    tmp_ortho = sharpen3x3all
+    tmp_ortho[1][1] = 8
+    tmp_ortho = tmp_ortho * k
+    tmp_ortho[1][1] = tmp_ortho[1][1]+9
+    tmp_ortho = tmp_ortho /9
+    img_out = cv2.filter2D(img, -1, tmp_ortho )
+
+    return img_out
+
+
 
 sharpen3x3ortho = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 sharpen3x3all = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
@@ -49,40 +72,79 @@ extension = input_file[-4:]
 input_file = input_file[:-4]
 
 # # circuit modifications
+new_img = img
+finalized_img_blurred = cv2.medianBlur(img,3)
+finalized_img = finalized_img_blurred
+finalized_img_sharpened = sharpen3o(finalized_img_blurred,5)
+finalized_img = finalized_img_sharpened
+finalized_img  = cv2.addWeighted(finalized_img_blurred, 0.75, finalized_img_sharpened, 0.25, 0)
+
+# # golf modifications
 # new_img = img
-# finalized_img_blurred = cv2.medianBlur(img,3)
-# finalized_img_sharpened = sharpen(finalized_img_blurred,sharpen3x3ortho,1.5)
+# finalized_img_blurred = cv2.medianBlur(cv2.medianBlur(img,3),5)
+# finalized_img = finalized_img_blurred
+# finalized_img_sharpened = sharpen3o(finalized_img_blurred,5)
+# finalized_img = finalized_img_sharpened
 # finalized_img = cv2.addWeighted(finalized_img_blurred, 0.75, finalized_img_sharpened, 0.25, 0)
 
-# golf modifications
-new_img = img
-finalized_img = img
-finalized_img_blurred = cv2.medianBlur(img,3)
 
-test_img  = cv2.medianBlur(cv2.medianBlur(img,3),5)
-test_img2 = cv2.medianBlur(img,5)
-test_img3 = cv2.medianBlur(cv2.medianBlur(img,5),3)
+# # pots modifications
+# new_img = img
+# finalized_img = img
+# finalized_img_sharp = sharpen3o(finalized_img,22)
+
+# rainbow modifications
+# new_img = img
+# finalized_img = img
+# finalized_img = cv2.bilateralFilter(img, 20,30,120)
 
 
-# test_img  = cv2.bilateralFilter(img,5,5,5)
-# test_img2 = cv2.bilateralFilter(img,5,10,10)
-# test_img3 = cv2.bilateralFilter(img,5,15,15)
+# ------------------
+# test_img  = cv2.medianBlur(cv2.medianBlur(img,3),5)
+# test_img2 = cv2.medianBlur(img,5)
+# test_img3 = cv2.medianBlur(cv2.medianBlur(img,5),3)
 
+
+# test_img  = cv2.bilateralFilter(img, 20,30,120)
+# test_img2 = cv2.bilateralFilter(img, 40,30,120)
+# test_img3 = cv2.bilateralFilter(img, 60,30,120)
+
+# test_img  = sharpen3o(finalized_img,1)
+# test_img2 = sharpen3o(finalized_img,3)
+# test_img3 = sharpen3o(finalized_img,5)
+
+# test_img  = sharpen3a(finalized_img,15)
+# test_img2 = sharpen3a(finalized_img,22)
+# test_img3 = sharpen3a(finalized_img,30)
+
+# test_img  = sharpen(finalized_img,sharpen3x3ortho,1)
+# test_img2 = sharpen(finalized_img,sharpen3x3all,1)
+# test_img3 = sharpen3o(finalized_img,2)
+
+# test_img  = sharpen(finalized_img,sharpen3x3ortho,1)
+# test_img2 = sharpen(finalized_img,sharpen3x3ortho,1.5)
+# test_img3 = sharpen(finalized_img,sharpen5x5,1)
+
+# test_img  = cv2.addWeighted(finalized_img_blurred, 0.75, finalized_img_sharpened, 0.25, 0)
+# test_img2 = cv2.addWeighted(finalized_img_blurred, 0.6, finalized_img_sharpened, 0.4, 0)
+# test_img3 = cv2.addWeighted(finalized_img_blurred, 0.9, finalized_img_sharpened, 0.1, 0)
 
 cv2.imwrite(input_file+"-improved"+extension, finalized_img)
 
 # creating empty named windows for later population
 cv2.namedWindow("input", cv2.WINDOW_NORMAL )
 cv2.namedWindow("corrected", cv2.WINDOW_NORMAL )
-cv2.namedWindow("corrected2", cv2.WINDOW_NORMAL )
-cv2.namedWindow("corrected3", cv2.WINDOW_NORMAL )
+# cv2.namedWindow("corrected2", cv2.WINDOW_NORMAL )
+# cv2.namedWindow("corrected3", cv2.WINDOW_NORMAL )
 
 
 
-cv2.imshow("input", finalized_img)
-cv2.imshow("corrected", test_img)
-cv2.imshow("corrected2", test_img2)
-cv2.imshow("corrected3", test_img3)
+cv2.imshow("input", img)
+# cv2.imshow("input", finalized_img)
+cv2.imshow("corrected", finalized_img)
+# cv2.imshow("corrected", test_img)
+# cv2.imshow("corrected2", test_img2)
+# cv2.imshow("corrected3", test_img3)
 
 
 
