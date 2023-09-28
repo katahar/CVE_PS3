b072a1485e9255a5535d8f646d48f5f035687952
Rishi Basdeo <32602245+katahar@users.noreply.github.com>
2023-09-28 09:48:53 -0400

    pcb complete

--- filters.py
+++ filters.py
@@ -20,9 +20,71 @@ cv2.addWeighted().
 Note to self: be sure to run "conda activate cve" before running this file
 
 '''
-
-
 import cv2
+import os
 import numpy as np
-import argparse
-# import matplotlib.pyplot as plt
+
+def sharpen(img, kernel, k):
+    img_out = cv2.filter2D(img, -1, kernel*k )
+    return img_out
+
+sharpen3x3ortho = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+sharpen3x3all = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+sharpen5x5 = np.array([[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, 25, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]])
+# sharpen5x5a = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+
+    
+
+# get file input
+print("Greetings! What photo would you like to edit? Your options are: ")
+for file_names in os.listdir():
+    if file_names.endswith(".png") or file_names.endswith(".jpg"):
+        print("\t" + file_names)
+input_file = input("\n input file: ")
+img = cv2.imread(input_file)
+
+# extracting extension and file base name
+extension = input_file[-4:]
+input_file = input_file[:-4]
+
+# # circuit modifications
+# new_img = img
+# finalized_img_blurred = cv2.medianBlur(img,3)
+# finalized_img_sharpened = sharpen(finalized_img_blurred,sharpen3x3ortho,1.5)
+# finalized_img = cv2.addWeighted(finalized_img_blurred, 0.75, finalized_img_sharpened, 0.25, 0)
+
+# golf modifications
+new_img = img
+finalized_img = img
+finalized_img_blurred = cv2.medianBlur(img,3)
+
+test_img  = cv2.medianBlur(cv2.medianBlur(img,3),5)
+test_img2 = cv2.medianBlur(img,5)
+test_img3 = cv2.medianBlur(cv2.medianBlur(img,5),3)
+
+
+# test_img  = cv2.bilateralFilter(img,5,5,5)
+# test_img2 = cv2.bilateralFilter(img,5,10,10)
+# test_img3 = cv2.bilateralFilter(img,5,15,15)
+
+
+cv2.imwrite(input_file+"-improved"+extension, finalized_img)
+
+# creating empty named windows for later population
+cv2.namedWindow("input", cv2.WINDOW_NORMAL )
+cv2.namedWindow("corrected", cv2.WINDOW_NORMAL )
+cv2.namedWindow("corrected2", cv2.WINDOW_NORMAL )
+cv2.namedWindow("corrected3", cv2.WINDOW_NORMAL )
+
+
+
+cv2.imshow("input", finalized_img)
+cv2.imshow("corrected", test_img)
+cv2.imshow("corrected2", test_img2)
+cv2.imshow("corrected3", test_img3)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
